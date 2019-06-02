4602d372b98f2c82ff8bb7a9b75387bd250120e5
Paul <paul@elliptic.co>
2019-06-02 23:24:44 +0100

    exercises for Option

--- exercises/src/main/scala/fpinscala/errorhandling/Option.scala
+++ exercises/src/main/scala/fpinscala/errorhandling/Option.scala
@@ -1,18 +1,35 @@
-package fpinscala.errorhandling
+//package fpinscala.errorhandling
+// To get this to work you need to paste it into the repl
+// Use :paste, paste it, and then use ctrl-D to exit paste mode
 
-
-import scala.{Option => _, Some => _, Either => _, _} // hide std library `Option`, `Some` and `Either`, since we are writing our own in this chapter
+import scala.{Option => _, Some => _,None => _,  Either => _, _} // hide std library `Option`, `Some` and `Either`, since we are writing our own in this chapter
 
 sealed trait Option[+A] {
-  def map[B](f: A => B): Option[B] = ???
+  def map[B](f: A => B): Option[B] = this match {
+    case None => None
+    case Some(x) => Some(f(x))
+  }
 
-  def getOrElse[B>:A](default: => B): B = ???
+  // here B is constrained to be a supertype of A
+  def getOrElse[B>:A](default: => B): B = this match {
+    case None => default
+    case Some(x) => x
+  }
 
-  def flatMap[B](f: A => Option[B]): Option[B] = ???
+  def flatMap[B](f: A => Option[B]): Option[B] = this match {
+    case None => None
+    case Some(x) => f(x)
+  }
 
-  def orElse[B>:A](ob: => Option[B]): Option[B] = ???
+  def orElse[B>:A](ob: => Option[B]): Option[B] = this match {
+    case None => ob
+    case Some(x) => Some(x)
+  }
 
-  def filter(f: A => Boolean): Option[A] = ???
+  def filter(f: A => Boolean): Option[A] = this match {
+    case Some(x) => if (f(x)) Some(x) else None
+    case None => None
+  }
 }
 case class Some[+A](get: A) extends Option[A]
 case object None extends Option[Nothing]
@@ -38,11 +55,41 @@ object Option {
   def mean(xs: Seq[Double]): Option[Double] =
     if (xs.isEmpty) None
     else Some(xs.sum / xs.length)
-  def variance(xs: Seq[Double]): Option[Double] = ???
 
-  def map2[A,B,C](a: Option[A], b: Option[B])(f: (A, B) => C): Option[C] = ???
+  def variance(xs: Seq[Double]): Option[Double] =
+    mean(xs).flatMap(m => mean(xs.map(x => math.pow(x - m, 2))))
 
-  def sequence[A](a: List[Option[A]]): Option[List[A]] = ???
+  def map2[A,B,C](a: Option[A], b: Option[B])(f: (A,B) => C): Option[C] =
+    (a,b) match {
+      case (_, None) => None
+      case (None, _) => None
+      case (Some(x), Some(y)) => Some(f(x, y))
+    }
 
-  def traverse[A, B](a: List[A])(f: A => Option[B]): Option[List[B]] = ???
+  def sequence[A](xs: List[Option[A]]): Option[List[A]] =
+    xs.foldRight(Some(Nil): Option[List[A]])((x, acc) => (acc, x) match {
+      case (None, _) => None
+      case (_, None) => None
+      case (Some(lst), Some(y)) => Some(y::lst)
+    })
+
+  // this is a much more sensible implementation and it fully demonstrates how useful map2 is!
+  def sequence_1[A](xs: List[Option[A]]): Option[List[A]] =
+    xs.foldRight(Some(Nil): Option[List[A]])((x,y) => map2(x,y)(_ :: _))
+
+  def traverse[A,B](xs: List[A])(f: A => Option[B]): Option[List[B]] =
+    xs.foldRight(Some(Nil): Option[List[B]])((x, acc) => acc.map(lst => f(x) :: lst))
+
+  def traverse_1[A, B](lst: List[A])(f: A => Option[B]): Option[List[B]] =
+    lst.foldRight[Option[List[B]]](Some(Nil))((x, xs) => map2(f(x), xs)(_ :: _))
+
+  def sequenceViaTraverse[A](xs: List[Option[A]]): Option[List[A]] =
+    traverse(xs)(x => x)
+
+  def map2_2[A,B,C](a: Option[A], b: Option[B])(f: (A, B) => C): Option[C] =
+    for {
+      aa <- a
+      bb <- b
+    } yield f(aa, bb)
+
 }
