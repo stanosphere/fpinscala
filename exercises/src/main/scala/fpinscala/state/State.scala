382b793f622a26aee1129a3075586286db3da1b1
Paul <paul@elliptic.co>
2019-06-09 19:43:35 +0100

    random list of ints

--- exercises/src/main/scala/fpinscala/state/State.scala
+++ exercises/src/main/scala/fpinscala/state/State.scala
@@ -1,5 +1,7 @@
 package fpinscala.state
 
+import scala.Option
+import scala.Some
 
 trait RNG {
   def nextInt: (Int, RNG) // Should generate a random `Int`. We'll later define other functions in terms of `nextInt`.
@@ -36,15 +38,58 @@ object RNG {
     (math.abs(1 + x), gen)
   }
 
-  def double(rng: RNG): (Double, RNG) = ???
+  def double(rng: RNG): (Double, RNG) = {
+    val (x, gen) = nonNegativeInt(rng)
+    (x.toDouble / Int.MaxValue, gen)
+  }
 
-  def intDouble(rng: RNG): ((Int,Double), RNG) = ???
+  def intDouble(rng: RNG): ((Int,Double), RNG) = {
+    val (myInt, gen) = rng.nextInt
+    val (myDouble, gen2) = double(gen)
+    ((myInt, myDouble), gen2)
+  }
 
-  def doubleInt(rng: RNG): ((Double,Int), RNG) = ???
+  def doubleInt(rng: RNG): ((Double,Int), RNG) = {
+    val ((myInt, myDouble), gen) = intDouble(rng)
+    ((myDouble, myInt), gen)
+  }
 
-  def double3(rng: RNG): ((Double,Double,Double), RNG) = ???
+  def double3(rng: RNG): ((Double,Double,Double), RNG) = {
+    val (d1, rng1) = double(rng)
+    val (d2, rng2) = double(rng1)
+    val (d3, rng3) = double(rng2)
+    ((d1, d2, d3), rng3)
+  }
 
-  def ints(count: Int)(rng: RNG): (List[Int], RNG) = ???
+  // it's a bit naughty to define this here, should be a listy boi
+  def unfoldRight[A,S](z: S)(f: S => Option[(A,S)]): List[A] =
+    f(z) match {
+      case Some((x, s)) => x :: unfoldRight(s)(f)
+      case None => Nil
+    }
+
+  // this is bonkers and also intellij gets mad for no reason
+  // but it works at least!
+  def intsViaUnfoldRight(count: Int)(rng: RNG): (List[Int], RNG) =
+    (unfoldRight((rng, count))({
+      case (_, 0) => None
+      case (_rng, cnt) => {
+        val (x, rngNext) = _rng.nextInt
+        Some((x, (rngNext, cnt - 1)))
+      }
+    }), rng)
+
+  // let's try to be tail recursive af (remember to decrement the count)
+  def ints(count: Int)(rng: RNG): (List[Int], RNG) = {
+    def go(i: Int, lst: List[Int], gen: RNG): (List[Int], RNG) = {
+      if (i == 0) (lst, gen)
+      else {
+        val (x, gen2) = gen.nextInt
+        go(i - 1, x :: lst, gen2)
+      }
+    }
+    go(count, Nil, rng)
+  }
 
   def map2[A,B,C](ra: Rand[A], rb: Rand[B])(f: (A, B) => C): Rand[C] = ???
 
