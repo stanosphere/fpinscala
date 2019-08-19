b4041a37f5735d7aed92e91881567315efc0f1cb
Paul <paul@elliptic.co>
2019-08-19 21:25:11 +0100

    add list of N

---
+++ answers/src/main/scala/fpinscala/parsing/PlayingWithParsers.scala
@@ -0,0 +1,5 @@
+package fpinscala.parsing
+
+object PlayingWithParsers extends App{
+
+}
--- exercises/src/main/scala/fpinscala/parsing/Parsers.scala
+++ exercises/src/main/scala/fpinscala/parsing/Parsers.scala
@@ -15,6 +15,9 @@ trait Parsers[Parser[+_]] { self => // so inner classes may call methods of trai
   implicit def asStringParser[A](a: A)(implicit f: A => Parser[String]):
     ParserOps[String] = ParserOps(f(a))
 
+  // looks roughly like a traverse???
+  def listOfN[A](n: Int, p: Parser[A]): Parser[List[A]]
+
   case class ParserOps[A](p: Parser[A]) {
     def |[B>:A](p2: Parser[B]): Parser[B] = self.or(p,p2)
     def or[B>:A](p2: => Parser[B]): Parser[B] = self.or(p,p2)
